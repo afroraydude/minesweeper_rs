42b01071089be6df43e1f74b7c9ae236d7cdfda4
afroraydude <thomasray0@gmail.com>
2022-10-19 20:26:18 -0400

    difficulties

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@
 
 use eframe::egui;
 use eframe::run_native;
-use egui::FontDefinitions;
 use objs::Board;
-use objs::Tile;
 
 mod objs;
 
@@ -12,6 +10,7 @@ struct Minesweeper {
     board: Board,
     is_game_over: bool,
     is_game_won: bool,
+    game_started: bool,
 }
 
 impl Minesweeper {
@@ -19,6 +18,7 @@ impl Minesweeper {
         self.board = Board::new(width, height, mines);
         self.is_game_over = false;
         self.is_game_won = false;
+        self.game_started = true;
     }
 }
 
@@ -28,72 +28,93 @@ impl Default for Minesweeper {
             board: Board::new(10, 10, 10),
             is_game_over: false,
             is_game_won: false,
+            game_started: false,
         }
     }
 }
 
 impl eframe::App for Minesweeper {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+        if (self.game_started == true) {
+            egui::CentralPanel::default().show(ctx, |ui| {
+                ui.heading("Minesweeper");
 
-            ui.heading("Minesweeper");
+                if self.is_game_over {
+                    ui.label("Game Over!");
+                    self.board.on_lost();
+                } else if self.is_game_won {
+                    ui.label("You won!");
+                }
 
-            if self.is_game_over {
-                ui.label("Game Over!");
-                self.board.on_lost();
-            } else if self.is_game_won {
-                ui.label("You won!");
-            }
-
-            // display the board
-            for y in 0..self.board.get_height() as usize {
-                ui.horizontal(|ui| {
-                    for x in 0..self.board.get_width() as usize {
-                        let tile = self.board.get_tile(x, y);
-                        let mut label = String::new();
-                        if tile.is_revealed {
-                            if tile.is_mine {
-                                // bomb emoji
-                                label = "💣".to_string();
+                // display the board
+                for y in 0..self.board.get_height() as usize {
+                    ui.horizontal(|ui| {
+                        for x in 0..self.board.get_width() as usize {
+                            let tile = self.board.get_tile(x, y);
+                            let mut label = String::new();
+                            if tile.is_revealed {
+                                if tile.is_mine {
+                                    // bomb emoji
+                                    label = "💣".to_string();
+                                } else {
+                                    label = format!(" {} ", tile.adjacent_mines);
+                                }
+                            } else if tile.is_flagged {
+                                // flag emoji
+                                label = "🚩".to_string();
                             } else {
-                                label = format!(" {} ", tile.adjacent_mines);
+                                // blank emoji
+                                label = "⬜".to_string();
                             }
-                        } else if tile.is_flagged {
-                            // flag emoji
-                            label = "🚩".to_string();
-                        } else {
-                            // blank emoji
-                            label = "⬜".to_string();
-                        }
 
-                        let button = ui.add_enabled(!self.is_game_over, egui::Button::new(label));
+                            let button =
+                                ui.add_enabled(!self.is_game_over, egui::Button::new(label));
 
-                        if button.clicked() {
-                            self.is_game_over = self.board.select_tile(x, y);
-                            self.is_game_won = self.board.is_win();
-                        }
+                            if button.clicked() {
+                                self.is_game_over = self.board.select_tile(x, y);
+                                self.is_game_won = self.board.is_win();
+                            }
 
-                        if button.secondary_clicked() {
-                            self.board.flag_tile(x, y);
+                            if button.secondary_clicked() {
+                                self.board.flag_tile(x, y);
+                            }
                         }
+                    });
+                }
+            });
+        } else {
+            egui::CentralPanel::default().show(ctx, |ui| {
+                ui.heading("Minesweeper");
+                ui.label("Welcome to Minesweeper!");
+                ui.label("Select a difficulty to begin.");
+                ui.horizontal(|ui| {
+                    let b1 = ui.add_enabled(!self.is_game_over, egui::Button::new("Easy"));
+                    let b2 = ui.add_enabled(!self.is_game_over, egui::Button::new("Medium"));
+                    let b3 = ui.add_enabled(!self.is_game_over, egui::Button::new("Hard"));
+                    
+                    if b1.clicked() {
+                        self.new_board(10, 10, 10);
+                    }
+
+                    if b2.clicked() {
+                        self.new_board(15, 15, 30);
+                    }
+
+                    if b3.clicked() {
+                        self.new_board(25, 25, 50);
                     }
                 });
-            }
-        });
+            });
+        }
     }
 }
 
 fn main() {
-    let mut minesweeper = Minesweeper {
-        board: Board::new(10, 10, 10),
-        is_game_over: false,
-        is_game_won: false,
-    };
-
     let options = eframe::NativeOptions::default();
 
     run_native(
-        "Minesweeper", 
-        options, 
-        Box::new(|ctx| Box::new(Minesweeper::default())));
+        "Minesweeper",
+        options,
+        Box::new(|ctx| Box::new(Minesweeper::default())),
+    );
 }
